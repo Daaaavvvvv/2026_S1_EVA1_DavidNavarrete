602e6d223ca6166bfa63303360ac2898bcdaa3de
David Navarrete <dav.navarreteo@duocuc.cl>
2026-04-02 12:07:43 -0300

    Service terminado

--- src/main/java/cl/duoc/DavidNavarete/service/MediTurnService.java
+++ src/main/java/cl/duoc/DavidNavarete/service/MediTurnService.java
@@ -1,5 +1,49 @@
 package cl.duoc.DavidNavarete.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cl.duoc.DavidNavarete.model.MediTurnModel;
+import cl.duoc.DavidNavarete.repository.MediTurnRepository;
+
+@Service
+
 public class MediTurnService {
+    @Autowired
+    private MediTurnRepository mediTurnRepository;
+    public MediTurnService(MediTurnRepository mediTurnRepository){
+        this.mediTurnRepository = mediTurnRepository;
+    }
+    public List<MediTurnModel>getAllMediTurnModel(){
+        return mediTurnRepository.findAll();
+    }
+    public Optional <MediTurnModel>getMediTurnModelByRut(Long rut){
+        return mediTurnRepository.findByRut(rut);
+
+    }
+    public MediTurnModel createMediTurnModel(MediTurnModel mediTurnModel){
+        return mediTurnRepository.save(mediTurnModel);
+
+    }
+    public Optional<MediTurnModel>updateMediTurnModel(Long rut, MediTurnModel mediTurnModel){
+        if (mediTurnRepository.existsByRut(rut)){
+            mediTurnModel.setRut(rut);
+            return Optional.of(mediTurnRepository.save(mediTurnModel));
+
+        }
+        return Optional.empty();
+    }
+    public boolean delete(Long rut){
+        if(mediTurnRepository.existsByRut(rut)){
+            mediTurnRepository.deleteByRut(rut);
+            return true;
+        }
+        return false;
+    }
+    
+
 
 }
